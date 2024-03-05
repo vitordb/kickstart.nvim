ba04049d717ac84a8c6ca37b777e4ef3a9a69f4d
Vítor Duarte Buchweitz <dbvitor@gmail.com>
2024-03-05 12:47:52 -0300

    add nvim-go

--- init.lua
+++ init.lua
@@ -255,8 +255,25 @@ require('lazy').setup {
   {
     'github/copilot.vim',
     config = function()
+      vim.cmd 'Copilot enable'
+
       -- Configurações adicionais do Copilot podem ser adicionadas aqui, se necessário.
     end,
+  },
+
+  {
+    'ray-x/go.nvim',
+    dependencies = { -- optional packages
+      'ray-x/guihua.lua',
+      'neovim/nvim-lspconfig',
+      'nvim-treesitter/nvim-treesitter',
+    },
+    config = function()
+      require('go').setup()
+    end,
+    event = { 'CmdlineEnter' },
+    ft = { 'go', 'gomod' },
+    build = ':lua require("go.install").update_all_sync()', -- if you need to install/update all binaries
   },
 
   {
